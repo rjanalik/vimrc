f561dfbf7da750c8bd4141d72701a8e3409fe9a7
Radim Janalík <radim.janalik@gmail.com>
2016-09-30 16:38:23 +0200

    Modified isntall_vimrc.sh
    Now installs plugins in vim and compiles YouCompleteMe.

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -5,3 +5,11 @@ ln -s ~/.vim/vimrc.vim ~/.vimrc
 
 # clone Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install plugins in vim
+vim +PluginInstall +qall
+
+# install YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+cd -
